b132cbd1f365daacb3a19f15a42e0405434bc0b5
Ilia Kuznetcov <x-i-ll@hotmail.com>
2025-04-25 14:08:25 +0200

    Initial commit

---
+++ Common/HelloServiceImpl.cs
@@ -0,0 +1,17 @@
+﻿using GodotGrpcExperiment.Common.Proto;
+
+using Grpc.Core;
+
+namespace GodotGrpcExperiment.Common;
+
+public class HelloServiceCore : HelloService.HelloServiceBase
+{
+    public override Task<HelloResponse> Hello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloResponse
+        {
+            Greeting = $"Hello {request.Name}!",
+            UnixTime = (ulong)DateTimeOffset.UtcNow.ToUnixTimeSeconds()
+        });
+    }
+}
